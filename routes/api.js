09e3f34083a4c98a9b7457e2e952793cacb079a7
difurung <dfurung@hotmail.com>
2023-02-20 13:59:24 -0500

    Added gitignore and server.js. Started routes.

---
+++ routes/api.js
@@ -0,0 +1,18 @@
+// exports
+const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+const uniqueID = require("uniqid");
+
+// Get route
+
+
+
+// Post route
+
+
+
+// Delete
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const api = require('./routes/api')
+const html = require('./routes/html')
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/api', api);
+app.use('/', html);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} `)
+);
